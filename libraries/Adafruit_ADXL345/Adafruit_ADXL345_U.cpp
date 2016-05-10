89da002567cda584e622f5935664cd33ad7955e7
koo5 <running.sun@gmail.com>
2016-05-10 20:17:46 +0200

    SPI_MODE + zachyceni cyklu

--- libraries/Adafruit_ADXL345/Adafruit_ADXL345_U.cpp
+++ libraries/Adafruit_ADXL345/Adafruit_ADXL345_U.cpp
@@ -46,17 +46,58 @@
   Wire.write((uint8_t)x);
 }
 
+
 /**************************************************************************/
 /*!
     @brief  Abstract away SPI receiver & transmitter
 */
 /**************************************************************************/
 
-/**************************************************************************/
-/*!
-    @brief  Writes 8-bits to the specified destination register
+/*
+static uint8_t spixfer(uint8_t clock, uint8_t miso, uint8_t mosi, uint8_t data) {
+  uint8_t reply = 0;
+  for (int i=7; i>=0; i--) {
+    reply <<= 1;
+    digitalWrite(clock, LOW);
+    digitalWrite(mosi, data & (1<<i));
+    digitalWrite(clock, HIGH);
+    if (digitalRead(miso)) 
+      reply |= 1;
+  }
+  return reply;
+}
+
+void Adafruit_ADXL345_Unified::writeRegister(uint8_t reg, uint8_t value) {
+  if (_i2c) {
+    Wire.beginTransmission(ADXL345_ADDRESS);
+    i2cwrite((uint8_t)reg);
+    i2cwrite((uint8_t)(value));
+    Wire.endTransmission();
+  } else {
+    digitalWrite(_cs, LOW);
+    spixfer(_clk, _di, _do, reg);
+    spixfer(_clk, _di, _do, value);
+    digitalWrite(_cs, HIGH);
+  }
+}
+
+uint8_t Adafruit_ADXL345_Unified::readRegister(uint8_t reg) {
+  if (_i2c) {
+    Wire.beginTransmission(ADXL345_ADDRESS);
+    i2cwrite(reg);
+    Wire.endTransmission();
+    Wire.requestFrom(ADXL345_ADDRESS, 1);
+    return (i2cread());
+  } else {
+    reg |= 0x80; // read byte
+    digitalWrite(_cs, LOW);
+    spixfer(_clk, _di, _do, reg);
+    uint8_t reply = spixfer(_clk, _di, _do, 0xFF);
+    digitalWrite(_cs, HIGH);
+    return reply;
+  }  
+}
 */
-/**************************************************************************/
 void Adafruit_ADXL345_Unified::writeRegister(uint8_t reg, uint8_t value) {
   if (_i2c) {
     Wire.beginTransmission(ADXL345_ADDRESS);
@@ -71,11 +112,6 @@ void Adafruit_ADXL345_Unified::writeRegister(uint8_t reg, uint8_t value) {
   }
 }
 
-/**************************************************************************/
-/*!
-    @brief  Reads 8-bits from the specified register
-*/
-/**************************************************************************/
 uint8_t Adafruit_ADXL345_Unified::readRegister(uint8_t reg) {
   if (_i2c) {
     Wire.beginTransmission(ADXL345_ADDRESS);
--- vibrometr1/vibrometr1.ino
+++ vibrometr1/vibrometr1.ino
@@ -40,6 +40,53 @@ void init_ram(void)
 }
 
 
+void test_ram() {
+  uint32_t address = 0;
+  const uint32_t bs = 1024L*256L;
+  Serial.println(F("sram test:"));
+  for(address = 0; address < ramsz; address+=bs)
+  {
+     Serial.print(address);
+     Serial.print(":");
+/*
+Mode  Clock Polarity (CPOL) Clock Phase (CPHA)
+SPI_MODE0 0 0
+SPI_MODE1 0 1
+*/
+    digitalWrite(sram.cs_pin, LOW);
+    SPI.transfer(SRAM_WRITE);
+    SPI.transfer((address >> 16) & 0xFF);
+    SPI.transfer((address >> 8)  & 0xFF);
+    SPI.transfer(address & 0xFF);
+    uint32_t d;
+    for(d = 0; d < bs; d++)
+    {
+      SPI.transfer(d);
+    }
+    digitalWrite(sram.cs_pin, HIGH);
+
+    Serial.print("..");
+
+    digitalWrite(sram.cs_pin, LOW);
+    SPI.transfer(SRAM_READ);
+    SPI.transfer((address >> 16) & 0xFF);
+    SPI.transfer((address >> 8)  & 0xFF);
+    SPI.transfer(address & 0xFF);
+    for(d = 0; d < bs; d++)
+    {
+      byte data = SPI.transfer(0);
+      byte exptd = (byte)d;
+      if (data != exptd)
+      {
+        Serial.print(data);
+        Serial.print("!=");
+        Serial.println(exptd);
+      }
+    }
+    digitalWrite(sram.cs_pin, HIGH);
+    Serial.println(F("done"));
+  }
+}
 
 
 
@@ -93,8 +140,8 @@ void acc_twi_flush(unsigned int count = 50)
 
 
 bool done;
-long long unsigned int address = 0;
-long long unsigned int toread;
+long unsigned int address = 0;
+long unsigned int toread;
 
 
 const unsigned int bufsiz = 6;
@@ -142,26 +189,42 @@ void test_acc_spi(Acc &acc)
 
 
   acc.writeRegister(ADXL345_REG_POWER_CTL, 0b0000);
+  Serial.println("flush..");
 
   acc_spi_flush();
   
   do_read = false;
   done = false;
   address = 0;
-    Serial.println((long)digitalRead(3));  
+  Serial.println((long)digitalRead(3));  
   attachInterrupt(digitalPinToInterrupt(3), spi_int, FALLING);  
-  
+  Serial.println("sampling..");
   acc.writeRegister(ADXL345_REG_POWER_CTL, 0b1000);   // Enable measurements
 }
+
+volatile unsigned int ticks;
+volatile unsigned int old_ticks = 65536;
+volatile unsigned long periods = 0;
 
 void spi_int(void) 
 {
   do_read = true;
-  Serial.println(F("xxx"));
+  ticks = TCNT1;
+  if (ticks < old_ticks)
+  {
+    periods++;
+  }
+  old_ticks = ticks;
 }
 
 void setup(void) 
 {
+  TIMSK0 = 0;
+  TIMSK1 = 0;
+  TCCR1A = 0;
+  TCCR1B = 1;
+  
+    
   Serial.begin(115200);
   
   digitalWrite(csram, 1);
@@ -171,15 +234,14 @@ void setup(void)
   pinMode(csram, 1);
   pinMode(cssd, 1);
   pinMode(csacc, 1);
-
-  digitalWrite(csram, 1);
-  digitalWrite(cssd, 1);
-  digitalWrite(csacc, 1);  
-  
-  SPI.setClockDivider(SPI_CLOCK_DIV128); // The maximum SPI clock speed is 5 MHz with 100 pF
+ 
+  SPI.setClockDivider(SPI_CLOCK_DIV4); // The maximum SPI clock speed is 5 MHz with 100 pF
   SPI.begin();//Transaction(sramspi);
 
   init_ram();
+  test_ram();
+  
+  SPI.setDataMode(SPI_MODE3);
 
   divider(); 
   
@@ -202,11 +264,8 @@ void divider(void)
 
 void loop(void)
 {
-  while(true)
-  {
     //Serial.println((long)digitalRead(3));  
     spi_read();
-  }
 }
  
 inline void spi_read(void)
@@ -214,6 +273,11 @@ inline void spi_read(void)
   if(do_read)
   {
     do_read = false;
+    /*if (!(address & 0b11111111))
+    {
+       Serial.println(address );
+    }*/
+    Serial.println(ticks + periods * 65536);
     address += 6;
     if (address == toread)
     {
@@ -221,11 +285,8 @@ inline void spi_read(void)
      done = true;
      Serial.println(F("done"));
     }
-    if (!(address & 0b11111111))
-    {
-       Serial.println(F("."));
-    }
     
+  
     for(int i = 0; i < 10; i++)
     {
     char buf[6];
@@ -242,10 +303,12 @@ inline void spi_read(void)
     //Serial.println(entries);
     if (entries > 30) 
       Serial.println(F("overflow"));
-    digitalWrite(csacc, HIGH);
     }
+
+    digitalWrite(csacc, HIGH);
     
-    /*
+    SPI.setDataMode(SPI_MODE0);  
+    
     digitalWrite(sram.cs_pin, LOW);
     SPI.transfer(SRAM_WRITE);
     SPI.transfer((address >> 16) & 0xFF);
@@ -256,7 +319,8 @@ inline void spi_read(void)
       SPI.transfer(buf[a]);
     } 
     digitalWrite(sram.cs_pin, HIGH); 
-    */
+    
+    SPI.setDataMode(SPI_MODE3);
   }
 }
 
